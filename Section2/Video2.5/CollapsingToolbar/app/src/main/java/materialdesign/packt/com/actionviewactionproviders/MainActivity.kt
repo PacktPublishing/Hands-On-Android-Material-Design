f31bd0d0405871ff3bba7790ed10034236c7ffb4
PacktDwayne <dwayned@packtpub.com>
2019-01-30 10:40:18 +0530

    Code files added

---
+++ Section2/Video2.5/CollapsingToolbar/app/src/main/java/materialdesign/packt/com/actionviewactionproviders/MainActivity.kt
@@ -0,0 +1,16 @@
+package materialdesign.packt.com.CollapsingToolbar
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.activity_main_toolbar))
+    }
+}

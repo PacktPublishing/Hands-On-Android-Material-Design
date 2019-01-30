97f5d423b7a3d0cfcc607059d5b72844b140a4ce
PacktDwayne <dwayned@packtpub.com>
2019-01-30 10:50:36 +0530

    Code files added

---
+++ Section2/Video2.9/BottomNavigation/app/src/main/java/materialdesign/packt/com/upbutton/MainActivity.kt
@@ -0,0 +1,42 @@
+package materialdesign.packt.com.upbutton
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.internal.BottomNavigationMenu
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.NavigationView
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.activity_main_toolbar))
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.main_BottomNavigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+   }
+   private val mOnNavigationItemSelectedListener = BottomNavigationView
+           .OnNavigationItemSelectedListener { item ->
+               when (item.itemId){
+                   R.id.action_account -> {
+                       main_txt.text = "Profile is Clicked"
+                       true
+                   }
+                   R.id.action_fav -> {
+                       main_txt.text = "Fav is Clicked"
+                       true
+                   }
+                   R.id.action_settings -> {
+                       main_txt.text = "Setting is Clicked"
+                       true
+                   }else ->{
+                   false
+               }
+               }
+
+   }
+
+}

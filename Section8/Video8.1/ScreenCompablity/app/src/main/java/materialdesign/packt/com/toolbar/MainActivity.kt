615d89f14b1ce3f995742fb3cdf96bb452e5b51b
PacktDwayne <dwayned@packtpub.com>
2019-01-30 15:21:24 +0530

    Code files added

---
+++ Section8/Video8.1/ScreenCompablity/app/src/main/java/materialdesign/packt/com/toolbar/MainActivity.kt
@@ -0,0 +1,13 @@
+package materialdesign.packt.com.toolbar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.activity_main_toolbar))
+    }
+}

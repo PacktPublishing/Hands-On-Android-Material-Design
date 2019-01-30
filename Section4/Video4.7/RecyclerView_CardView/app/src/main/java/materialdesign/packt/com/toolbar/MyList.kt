19a3b78f77fe7a3846c668900ac1665cff78461a
PacktDwayne <dwayned@packtpub.com>
2019-01-30 11:41:57 +0530

    Code files added

---
+++ Section4/Video4.7/RecyclerView_CardView/app/src/main/java/materialdesign/packt/com/toolbar/MyList.kt
@@ -0,0 +1,11 @@
+package materialdesign.packt.com.toolbar
+
+/**
+ * Created by ${Mona}
+ */
+class MyList{
+    var userName : String = ""
+    var userMsg : String = ""
+    var userImg : Int = 0
+
+}
---
+++ Section4/Video4.8/CollapsingT_RV_CV/app/src/main/java/materialdesign/packt/com/toolbar/MainAdapter.kt
@@ -0,0 +1,32 @@
+package materialdesign.packt.com.toolbar
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+/**
+ * Created by ${Mona}
+ */
+class MainAdapter(val items : ArrayList<MyList>, val context: Context)  : RecyclerView.Adapter<MainViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): MainViewHolder {
+
+        return MainViewHolder(LayoutInflater.from(context).inflate(R.layout.rec_item, parent, false))
+}
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder : MainViewHolder, position: Int) {
+        holder.tvUserName.text = items.get(position).userName
+        holder.tvUserMsg.text = items.get(position).userMsg
+        holder.tvUserImg.setImageResource(items.get(position).userImg)
+        holder.CvMain.setOnClickListener { Toast.makeText(context,"Hi the cv in the rv $position" ,Toast.LENGTH_SHORT).show() }
+    }
+
+}
